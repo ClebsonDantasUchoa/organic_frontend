5fef1afaabbad0a8b0172459a3dfbf4601eb8ccd
Clebson <clebsondantasuchoa@hotmail.com>
2019-11-19 21:00:04 -0300

    login view initialized

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="content">
+        aaa
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.content{
+    /* background: rgb(4,36,0); */
+    /* background: linear-gradient(90deg, rgba(4,36,0,1) 4%, rgba(26,121,9,1) 43%, rgba(0,255,59,1) 74%); */
+    height: 100%;
+    
+}
+</style>
