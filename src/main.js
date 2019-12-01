fb60424da949a6225a0f4221947b456be00cc416
Clebson <clebsondantasuchoa@hotmail.com>
2019-12-01 14:25:26 -0300

    Bulma, icons and css configurations

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bulma/bulma.sass'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
