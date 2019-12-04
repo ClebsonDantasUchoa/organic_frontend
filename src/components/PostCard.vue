fd0ea7f075e2e7b9545d3184e57959aafb6a53a3
Clebson <clebsondantasuchoa@hotmail.com>
2019-12-03 23:13:20 -0200

    Post input and initial modal comments

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,75 +1,119 @@
 <template>
-    <div class="postCardContent">
-        <div class="userDiv">
-            <img class="avatar" v-bind:src="post.autorImage" alt="">
-            <p>{{post.autor}}</p>
-        </div>
-        <div class="postContent">
-            <p>{{ post.message }}</p>
-            <img class="postImage" v-bind:src="post.postImage">
-        </div>
-    </div>    
+  <div class="postCardContent">
+    <div class="userDiv">
+      <img class="avatar" v-bind:src="post.autorImage" alt />
+      <p>{{post.autor}}</p>
+    </div>
+    <div class="postContent">
+      <p>{{ post.message }}</p>
+      <img class="postImage" v-bind:src="post.postImage" />
+    </div>
+    <div class="options">
+      <button class="button is-success is-outlined">Curtir</button>
+      <button class="button is-success" @click="setModalVisibilty" >Comentar</button>
+    </div>
+
+    <div class="modal" :class="{'activeModal':showModal}">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save changes</button>
+          <button class="button is-success" @click="setModalVisibilty" >Comentar</button>
+        </footer>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        post: Object
-    }
-}
+  props: {
+    post: Object
+  },
+  data(){
+      return {
+        showModal : false
+      }
+  },
+  methods: {
+      setModalVisibilty(){
+        this.showModal = !this.showModal    
+      }
+  }
+};
 </script>
 
 <style scoped>
-
-.postCardContent{
-    width: 30px;
-    margin: 15px 0px;
-    width: 100%;
+.activeModal{
     display: flex;
-    flex-direction: column;
-    border-width: 1px;
-    border-color:  rgb(23, 201, 100);
-    border-radius: 7px;
-    border-style: solid;
-    max-width: 340px;
 }
-.userDiv{
-    display: flex;
-    align-items: center;
-    margin-left: 30px;
-    margin-top: 10px;
+.postCardContent {
+  width: 30px;
+  margin: 15px 0px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-width: 1px;
+  border-color: rgb(23, 201, 100);
+  border-radius: 7px;
+  border-style: solid;
+  max-width: 340px;
 }
-.postContent p{
-    text-align: justify;
-    text-indent: 20px
+.userDiv {
+  display: flex;
+  align-items: center;
+  margin-left: 30px;
+  margin-top: 10px;
+  border-bottom: 1px;
 }
-.userDiv p{
-    margin-left: 10px;
-    color: black
+.postContent p {
+  text-align: justify;
+  text-indent: 20px;
+}
+.userDiv p {
+  margin-left: 10px;
+  color: black;
 }
 .postContent {
-    margin-top: 16px;
-    margin-left: 15px;
-    margin-right: 15px;
+  margin-top: 16px;
+  margin-left: 15px;
+  margin-right: 15px;
 }
 .avatar {
   border-radius: 50%;
   border-width: 10px;
-  border-color:  rgb(23, 201, 100);
+  border-color: rgb(23, 201, 100);
   width: 45px;
   height: 45px;
 }
-.postImage{
-    margin: 10px 0;
+.postImage {
+  margin: 10px 0;
 }
-@media all and (min-width: 400px){
-    .postCardContent{
-        max-width: 500px;
-    }
+.options {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 10px;
 }
-@media all and (max-width: 330px){
-    .postCardContent{
-        max-width: 290px;
-    }
+button {
+  background-color: white;
+  color: rgb(23, 201, 100);
+  margin-left: 10px;
+}
+@media all and (min-width: 400px) {
+  .postCardContent {
+    max-width: 500px;
+  }
+}
+@media all and (max-width: 330px) {
+  .postCardContent {
+    max-width: 290px;
+  }
 }
 </style>
---
+++ src/components/PostInput.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="post-input">
+        <div class="field">
+            <div class="control">
+                <textarea class="input is-success" type="text-area" placeholder="Escreva algo"  rows="5" cols="25">
+                </textarea>
+                <div class="level-left">
+                    <div class="level-item">
+                        <a class="button is-info">Postar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        
+    }
+}
+//height: 90px !important;
+</script>
+
+<style scoped>
+.post-input {
+    margin: 20px 0;
+    border-width: 5px;
+    border-color: black;
+    height: 100%;
+}
+textarea{
+    resize: none;
+    width: 450px !important;
+    min-height: 100%;
+    max-height: 100%;
+}
+.control{
+    display: relative;
+    height: 100%;
+}
+.button {
+    position: absolute;
+    right: 0;
+    border-radius: 100px;
+    margin-right: -40px;
+    background-color: rgb(23, 201, 100) !important;
+}
+@media all and (max-width: 400px){
+    textarea{
+        width: 270px !important;
+    }
+    .button{
+        font-size: 15px;
+        margin-right: -20px
+    }
+}
+</style>
