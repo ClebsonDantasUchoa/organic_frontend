3047f8a10423f3223723dd367794527263bd2c4b
Clebson <clebsondantasuchoa@hotmail.com>
2019-12-10 16:46:32 -0300

    Initial load community

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="groups">
     <h1 class="has-text-centered">Grupos</h1>
+    <div v-for="(user, key) in community" :key="key">
+      <p>
+        {{user.name}}
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data(){
+    return{
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      community: "search/getCommunity"
+    })
+  },
+
+  mounted(){
+    this.$store.dispatch("search/searchCommunity");
+  },
+
+  methods: {
+
+  }
+}
+</script>
